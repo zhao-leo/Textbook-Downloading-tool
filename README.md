0f2c71086d50dbc2360da3ec855dee7bb24be645
zhao-leo <18055219130@163.com>
2024-06-15 01:56:00 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # PEP-textbook-downloading-tool
+
+## 一个将PEP电子书下载下来的方法
+主要是通过抓取[pep](https://jc.pep.com.cn/)的电子书内容图片下载并合并为pdf
+运行在python3.11.5上
+## 运行
+```cmd
+git clone https://github.com/zhao-leo/PEP-textbook-downloading-tool.git
+cd PEP-textbook-downloading-tool
+pip install -r requirements.txt
+python main.py
+```
+## 愉快使用~
+
+图书版权归 人民教育出版社 所有
+
---
+++ main.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import requests
+from fpdf import FPDF
+from tqdm import tqdm
+import shutil
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import time
+
+def get_para(book_url):
+    response = requests.get(book_url)
+    time.sleep(1)
+    response.encoding = 'utf-8'  # 设置响应的编码为UTF-8，根据实际情况选择合适的编码
+    soup = BeautifulSoup(response.text, "html.parser")
+    title = soup.find('title')
+    print(f"Book Name: {title.text}")
+
+    parsed_url = urlparse(book_url)
+    path = parsed_url.path
+    id_part = path.split('/')[1]
+    print(f"Book Id:{id_part}")
+    return title.text, id_part
+
+def download_image(url, filename):
+    filepath = f".src/{filename}"
+    response = requests.get(url)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
+
+def generate_pdf(baseurl, start, end, pdf_filename):
+    if not os.path.exists('.src'):
+        os.makedirs('.src')
+    pdf = FPDF()
+    for i in tqdm(range(start, end + 1), desc="Generating PDF"):
+        image_url = f"{baseurl}{i}.jpg"
+        image_filename = f"{i}.jpg"
+        image_filepath = download_image(image_url, image_filename)
+        pdf.add_page()
+        pdf.image(image_filepath, x=0, y=0, w=210, h=297)
+    pdf.output(pdf_filename)
+
+def main():
+    print("Welcome To Use The PEP Textbook Downloading Tool")
+    print("What you need to do is to EXPOLRE https://jc.pep.com.cn/")
+    print("CHOOSE a textbook, COPY its URL and set START and END page")
+    BOOK_URL = input("Please input the book URL:")
+    START_PAGE = int(input("Please input the start page:"))
+    END_PAGE = int(input("Please input the end page:"))
+
+    BOOK_NAME,BOOK_ID = get_para(BOOK_URL)
+
+    baseurl = f"https://book.pep.com.cn/{BOOK_ID}/files/mobile/"
+
+    try:
+        generate_pdf(baseurl, START_PAGE, END_PAGE, f"{BOOK_NAME}.pdf")
+    except Exception as e:
+        print(f"Error: {e}")
+    else:
+        print(f"PDF generated successfully: {BOOK_NAME}.pdf")
+        shutil.rmtree('.src', ignore_errors=True)
+    os.system("pause")
+if __name__ == "__main__":
+    main()
