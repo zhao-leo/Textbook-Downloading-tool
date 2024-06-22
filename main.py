e8b2b51fc6b218b227a22b1c26dc99389c074565
zhao-leo <18055219130@163.com>
2024-06-22 17:32:34 +0800

    🦄 refactor(BNU和PEP部分都有修改): 将整体文件分开编写
    添加了北京师范大学书源，优化展示内容

--- main.py
+++ main.py
@@ -1,73 +1,37 @@
-# -*- coding: utf-8 -*-
-import requests
-from fpdf import FPDF
-from tqdm import tqdm
-import shutil
-import os
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-import time
+import book_source.bnu
+import book_source.pep
 
-def get_para(book_url):
-    response = requests.get(book_url)
-    time.sleep(1)
-    response.encoding = 'utf-8'  # 设置响应的编码为UTF-8，根据实际情况选择合适的编码
-    soup = BeautifulSoup(response.text, "html.parser")
-    title = soup.find('title')
-    print(f"Book Name: {title.text}")
-
-    parsed_url = urlparse(book_url)
-    path = parsed_url.path
-    id_part = path.split('/')[1]
-    print(f"Book Id:{id_part}")
-    return title.text, id_part
-
-def download_image(url, filename):
-    filepath = f".src/{filename}"
-    response = requests.get(url)
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
-    return filepath
-
-def generate_pdf(baseurl, start, end, pdf_filename):
-    if not os.path.exists('.src'):
-        os.makedirs('.src')
-    pdf = FPDF()
-    for i in tqdm(range(start, end + 1), desc="Generating PDF"):
-        image_url = f"{baseurl}{i}.jpg"
-        image_filename = f"{i}.jpg"
-        image_filepath = download_image(image_url, image_filename)
-        pdf.add_page()
-        pdf.image(image_filepath, x=0, y=0, w=210, h=297)
-    pdf.output(pdf_filename)
-
-def main():
-    BOOK_URL = input("Please input the book URL:")
-    START_PAGE = int(input("Please input the start page:"))
-    END_PAGE = int(input("Please input the end page:"))
-
-    BOOK_NAME,BOOK_ID = get_para(BOOK_URL)
-
-    baseurl = f"https://book.pep.com.cn/{BOOK_ID}/files/mobile/"
-
-    try:
-        generate_pdf(baseurl, START_PAGE, END_PAGE, f"{BOOK_NAME}.pdf")
-    except Exception as e:
-        print(f"Error: {e}")
+print("Welcome to the textbook downloader!")
+print("Now support the following sources:")
+print("1. PEP (https://jc.pep.com.cn/)")
+print("2. BNU (http://www.100875.com.cn/show/resource.html)")
+print("3. Exit (Exit the program.)")
+source = input("Please input the source of the book (1/2/3):")
+while True:
+    if source == "3":
+        print("Goodbye!")
+        break
+    elif source == "1":
+        book_source.pep.pep_main()
+    elif source == "2":
+        book_source.bnu.bnu_main()
     else:
-        print(f"PDF generated successfully: {BOOK_NAME}.pdf")
-        shutil.rmtree('.src', ignore_errors=True)
-    os.system("pause")
-if __name__ == "__main__":
-    i=1
-    print("Welcome To Use The PEP Textbook Downloading Tool")
-    print("What you need to do is to EXPOLRE https://jc.pep.com.cn/")
-    print("CHOOSE a textbook, COPY its URL and set START and END page")
-    while(i):
-        switch = input("Do you want to download a textbook? (Y/N)")
-        if switch == "Y" or switch == "y":
-            main()
-        else:
-            i=0
-            print("Thank you for using the PEP Textbook Downloading Tool")
-            os.system("pause")
+        print("Invalid source!")
+    if source == "1":
+        pressname = "PEP"
+        another = "BNU"
+    elif source == "2":
+        pressname = "BNU"
+        another = "PEP"
+    print("Do you want to download another book?")
+    print(f"If you still want to download {pressname} book,please input Enter,")
+    print(f"if you want to download {another} book,please input 1,")
+    print("if you want to exit,please input 3.")
+    sourcex = input("Please input your choice:")
+    if sourcex == "3":
+        source = "3"
+    elif sourcex == "1":
+        if source == "1":
+            source = "2"
+        elif source == "2":
+            source = "1"
